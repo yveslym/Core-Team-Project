ae15819f6b5de8540f19a263322de9b187b4981e
yves songolo <yves.songolo@gmail.com>
2017-11-01 17:29:42 -0700

    set plaid LinkKit manual setup on a--delegate

---
+++ Plaid framework test/Plaid framework test/AppDelegate.swift
@@ -0,0 +1,70 @@
+//
+//  AppDelegate.swift
+//  Plaid framework test
+//
+//  Created by Yveslym on 11/1/17.
+//  Copyright © 2017 Yveslym. All rights reserved.
+//
+
+import UIKit
+import LinkKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    /// function to configure the plaid api Kit when program launch
+    /// to minimize the api loading time
+    /// the configuration manual parameter
+    func configurePlaidLinkKit(){
+        let linkConfiguration = PLKConfiguration(key: "a26bacd40a288d215735a0cfcb1508", env: .sandbox, product: .auth)
+        linkConfiguration.clientName = "Link Demo"
+        PLKPlaidLink.setup(with: linkConfiguration) { (success, error) in
+            if (success) {
+                // Handle success here, e.g. by posting a notification
+                NSLog("Plaid Link setup was successful")
+                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "PLDPlaidLinkSetupFinished"), object: self)
+            }
+            else if let error = error {
+                NSLog("Unable to setup Plaid Link due to: \(error.localizedDescription)")
+            }
+            else {
+                NSLog("Unable to setup Plaid Link")
+            }
+        }
+        
+
+    }
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        self.configurePlaidLinkKit()
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
